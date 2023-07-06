7085f194e42dc15424cc47858795ed6d501fb92c
Котя <71443736+kotisoff@users.noreply.github.com>
2023-07-07 00:54:22 +0300

    trackid support.
    костыль для включения русских песен. Ну или конкретно той которую вы хотите, дальше будет поддержка (кривая) url

--- commands/music/yandexmusic.js
+++ commands/music/yandexmusic.js
@@ -27,6 +27,7 @@ const config = fileimport(path.join(__dirname, "../../configs/kot.music/yaconfig
 )
 
 const YaMusicApi = new ym.YMApi();
+YaMusicApi.init({ access_token: config.user.token, uid: config.user.uid });
 const YaWrapper = new ym.WrappedYMApi(YaMusicApi);
 
 const checkVoice = (interact) => {
@@ -37,31 +38,37 @@ const checkVoice = (interact) => {
         return discordv.joinVoiceChannel({ adapterCreator: interact.guild.voiceAdapterCreator, guildId: interact.guildId, channelId: interact.member.voice.channelId })
 }
 
-const playsound = async (interact, title, debug) => {
+const playsound = async (interact, title, debug, searchbyTrackId) => {
     await interact.reply("*Думоет...*")
     const connection = checkVoice(interact)
     const player = discordv.createAudioPlayer();
     let resource = discordv.AudioResource.prototype
     let out = ""
-    await YaMusicApi.init({access_token:config.user.token,uid:config.user.uid})
     interact.editReply("Поиск по: `" + title + "`")
-    const result = await YaMusicApi.searchTracks(title)
-    const titles = []
-    result.tracks.results.forEach(res => {
-        const artists = []
-        res.artists.forEach(artist => artists.push(artist.name))
-        titles.push(res.title + " - " + artists.join(", "))
-    })
-    interact.editReply("Результат:\n" + titles.join("\n"))
-    const track = result.tracks.results[0]
-    const stream = await YaWrapper.getMp3DownloadUrl(track.id, "low")
+    console.log("Поиск по: " + title)
+    let track
+    if (!searchbyTrackId) {
+        const result = await YaMusicApi.searchTracks(title, { page: 0 })
+        const titles = []
+        result.tracks.results.forEach(res => {
+            const artists = []
+            res.artists.forEach(artist => artists.push(artist.name))
+            titles.push(res.title + " - " + artists.join(", "))
+        })
+        interact.editReply("Результат:\n" + titles.join("\n"))
+        track = result.tracks.results[0]
+    } else {
+        track = (await YaMusicApi.getTrack(parseInt(title)))[0]
+    }
+    const stream = await YaWrapper.getMp3DownloadUrl(parseInt(track.id), "low")
     resource = discordv.createAudioResource(stream, { inlineVolume: true })
     const artists = []
     track.artists.forEach(a => artists.push(a.name))
     out = `Запущено: \`${track.title} - ${artists.join(", ")}\``
-    if(debug) {
-        const data = await YaWrapper.getMp3DownloadInfo(track.id, "low")
-        out = (`${out}\nPreview: \`${data.preview}\`\nTrackID: \`${track.id}\``)
+    if (debug) {
+        const data = await YaWrapper.getMp3DownloadInfo(parseInt(track.id), "low")
+        out+=(`\nPreview: \`${data.preview}\`\nTrackID: \`${track.id}\``)
+        if(data.preview) out+="\nУ вас нет Yandex Plus или использован некорректный токен!"
     }
     console.log(out)
     player.play(resource);
@@ -78,7 +85,7 @@ module.exports = {
                 .setDescription("Parameter")
                 .addChoices(
                     { name: "Проиграть", value: "play" },
-                    { name: "Проиграть с отладкой", value: "playdebug"},
+                    { name: "Проиграть с отладкой", value: "playdebug" },
                     { name: "Покинуть", value: "leave" }
                 )
                 .setRequired(true)
@@ -86,13 +93,28 @@ module.exports = {
         .addStringOption(o =>
             o.setName("songtitle")
                 .setDescription("Название песни.")
-        ),
+        )
+        .addNumberOption(o =>
+            o.setName("trackid")
+                .setDescription("Или ID песни. (Последнее число в ссылке. Пример: /album/0/track/АЙДИ)")),
     async iexec(interact, bot) {
         const param = await interact.options.getString("param")
         const title = await interact.options.getString("songtitle")
-        if (!param) return interact.reply("А если подумать?")
-        if (param === "play") return playsound(interact, title)
-        if (param === "playdebug") return playsound(interact, title, true)
-        if (param === "leave") { await interact.reply("Отключаемся."); const voice = await discordv.getVoiceConnection(interact.guildId); await voice.disconnect(); return await voice.destroy(); }
+        const trackid = await interact.options.getNumber("trackid")
+        if (trackid) {
+            if (param === "play") return playsound(interact, trackid, false, true)
+            if (param === "playdebug") return playsound(interact, trackid, true, true)
+        } else if (title) {
+            if (param === "play") return playsound(interact, title, false, false)
+            if (param === "playdebug") return playsound(interact, title, true, false)
+        }
+        if (param === "leave") {
+            await interact.reply("Отключаемся.");
+            const voice = await discordv.getVoiceConnection(interact.guildId);
+            await voice.disconnect();
+            return await voice.destroy();
+        } else {
+            return interact.reply("А если подумать?")
+        }
     }
 }
