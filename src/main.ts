e1d8e8bdd7c6ffc38f0256d90e8485432bbd6139
KotIsOff <kot252steam@gmail.com>
2024-10-11 23:16:59 +0300

    config not found fix

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,9 @@ import { timer } from "./core/Utils/reusedUtils";
 
 log.info("All modules loaded".gray);
 
-if (!fs.existsSync("./config.json")) {
+if (!fs.existsSync("../config.json")) {
   log.warn("Config not found.".gray);
-  fs.writeFileSync("./config.json", JSON.stringify(new Config()));
+  fs.writeFileSync("../config.json", JSON.stringify(new Config()));
   log.info("Created a new config!".green);
   process.exit(0);
 }
